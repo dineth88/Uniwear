0d280566f92f1071d2d82f471e2ecaa8f8058d9d
dineth88 <dinethoshitha00@gmail.com>
2024-09-02 03:03:45 +0530

    Changed Discover section

--- Frontend/src/pages/Discover/Discover.jsx
+++ Frontend/src/pages/Discover/Discover.jsx
@@ -4,39 +4,24 @@ import '../../index.css';
 import Dropdown from 'react-dropdown';
 import 'react-dropdown/style.css'; // Ensure to import the CSS for the Dropdown component
 import { useNavigate } from 'react-router-dom';
+import HoverCard from "@darenft/react-3d-hover-card";
+import "@darenft/react-3d-hover-card/dist/style.css";
 
 export default function Discover({setShowNav}){
 
-    const [university, setUniversity] = useState('');
-
-    const options = [
-        { value: 'SelectUni', label: 'Select Your University...' },
-        { value: 'UOV', label: 'University of Vavuniya' },
-        { value: 'UOP', label: 'University of Peradeniya' },
-        { value: 'UOC', label: 'University of Colombo' },
-        { value: 'UOJ', label: 'University of Jaffna' },
-        { value: 'UOR', label: 'University of Ruhuna' },
-        { value: 'UOK', label: 'University of Kelaniya' },
-        { value: 'UOM', label: 'University of Moratuwa' },
-        { value: 'USJ', label: 'University of Sri Jayawardenepura' },
-        { value: 'RUSL', label: 'Rajarata University of Sri Lanka' },
-        { value: 'EUSL', label: 'Eastern University of Sri Lanka' }
-    ];
-    const defaultOption = options[0];
+    
 
     return(
         <>
-            <div className="discover-div">
-                <form >
-                    <div>
-                        <Dropdown
-                            options={options}
-                            onChange={(option) => setUniversity(option.value)}
-                            value={defaultOption}
-                            className="form-control"
-                        />
-                    </div>
-                </form>
+            <div className="App">
+                <HoverCard scaleFactor={0.4}>
+                    <img src="https://s3.duellinksmeta.com/cards/60c2b3aba0e24f2d54a52a69_w360.webp" />
+                </HoverCard>
+            </div>
+            <div className="App">
+                <HoverCard scaleFactor={1.4}>
+                    <img src="https://s3.duellinksmeta.com/cards/60c2b3aba0e24f2d54a52a69_w360.webp" />
+                </HoverCard>
             </div>
         </>
     );
